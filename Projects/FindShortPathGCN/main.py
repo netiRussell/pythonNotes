b0fc7ce2d677590a9090701c5f9075ff17396565
Russell / Ruslan <forworkneti@gmail.com>
2024-07-29 12:25:29 -0700

    Looping over batches and data split implemented
    Dummy training loop is implemented(delete breaks to make it iterate over all the samples present in the data set twice)
    
    Dataset is now split into train and validation sets. Corresponding iterable objects initialized.

--- Projects/FindShortPathGCN/main.py
+++ Projects/FindShortPathGCN/main.py
@@ -11,26 +11,46 @@ from visualization import visualize
 # TODO: no fixed amount of steps
 # TODO: 10'000 dataset
 
+def split_data(dataset, val_ratio):
+  train_size = int(total_samples * (1.0 - val_ratio))
+  validation_size = total_samples - train_size
 
-# TODO 2: Load the data using dataLoader or smth like that
+  train_dataset, validation_dataset = torch.utils.data.random_split(dataset, [train_size, validation_size])
 
-# Hyper parameters and Data setup
+  trainLoader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+  validationLoader = DataLoader(validation_dataset, batch_size=100, shuffle=False)
+
+  return iter(trainLoader), iter(validationLoader)
+
+
+"""
+  ## Short path prediction algoritm by Ruslan Abdulin ##
+  Code to be executed starts here.
+"""
+
+# -- Hyper parameters and Data setup --
 dataset = FindShortPathDataset(root="./data")
 
-n_epoch = 2
-total_samples = len(dataset)
+n_epochs = 2
 batch_size = 20
+total_samples = len(dataset)
 n_iterations = ceil(total_samples/batch_size)
-print(f"Samples = {total_samples}, Iterations = {n_iterations}")
+# print(f"Samples = {total_samples}, Iterations = {n_iterations}")
 
-dataLoader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
-dataIter = iter(dataLoader)
+trainIter, validIter = split_data( dataset=dataset, val_ratio=0.2)
 
-# data = next(dataiter)
-# print(data[0].x, "\n", data[0].y)
 
-# Get info and visualization
+# -- Get info and visualization --
 visualize(dataset, False)
 
-# Training loop
 
+# -- Training loop --
+for epoch in range(n_epochs):
+  # One epoch
+  for batch in trainIter:
+    # One batch
+    for i in range(batch_size):
+      # One sample
+      print(batch[i].x, batch[i].y)
+    break
+  break
