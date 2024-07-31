5659ffb840b84414b49d558f6b61be07852d1b97
Russell / Ruslan <forworkneti@gmail.com>
2024-07-30 17:50:20 -0700

    Improved model. Still not efficient

--- Projects/FindShortPathGCN/main.py
+++ Projects/FindShortPathGCN/main.py
@@ -1,7 +1,8 @@
 import torch
 from math import ceil
 from torch_geometric.loader import DataLoader
-from torch.nn import Linear
+from torch.nn import Linear, LeakyReLU, ELU, Dropout
+import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
 from dataset import FindShortPathDataset
 from visualization import visualize
@@ -18,12 +19,11 @@ def split_data(dataset, val_ratio):
 
   train_dataset, validation_dataset = torch.utils.data.random_split(dataset, [train_size, validation_size])
 
-  trainLoader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+  trainLoader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
   validationLoader = DataLoader(validation_dataset, batch_size=100, shuffle=False)
 
   return trainLoader, validationLoader
 
-# TODO: apply mini-batching approach for training
 def trainSample(sample, criterion):
   out = model(sample.x, sample.edge_index)
   loss = criterion(out, sample.y)
@@ -40,7 +40,7 @@ def trainSample(sample, criterion):
 # -- Hyper parameters and Data setup --
 dataset = FindShortPathDataset(root="./data")
 
-n_epochs = 10
+n_epochs = 8
 batch_size = 25
 total_samples = len(dataset)
 n_iterations = ceil(total_samples/batch_size)
@@ -57,46 +57,72 @@ visualize(dataset, False)
 class Network(torch.nn.Module):
   def __init__(self, n_hidden_channels):
     super(Network, self).__init__()
+    torch.manual_seed(1234567)
     self.conv1 = GCNConv(dataset.num_features, n_hidden_channels)
     self.conv2 = GCNConv(n_hidden_channels, n_hidden_channels)
-    self.conv3 = GCNConv(n_hidden_channels, n_hidden_channels)
-    self.conv4 = GCNConv(n_hidden_channels, dataset.num_classes)
-    self.classifier = Linear(dataset.num_classes, 1)
+    self.conv3 = GCNConv(n_hidden_channels, 33)
+    self.conv4 = GCNConv(33, 11)
+    self.conv5 = GCNConv(11, 5)
+    self.conv6 = GCNConv(5, 3)
+    self.classifier = Linear(3, 1)
+    self.elu = ELU(alpha=1.0, inplace=False)
+    self.dropout05 = Dropout(p=0.5)
+    self.dropout02 = Dropout(p=0.2)
 
   def forward(self, x, edge_index):
-    # TODO: Different activation functions
     out = self.conv1(x, edge_index)
-    out = torch.tanh(self.conv2(out, edge_index))
-    out = torch.nn.LeakyReLU(self.conv3(out, edge_index))
-    out = torch.nn.LeakyReLU(self.conv4(out, edge_index))
-    out = torch.nn.ELU(self.classifier(out))
+    out = self.dropout02(out)
+    out = self.conv2(out, edge_index)
+    out = self.elu(out)
+    out = self.conv3(out, edge_index)
+    out = self.dropout05(out)
+    out = self.conv4(out, edge_index)
+    out = self.elu(out)
+    out = self.conv5(out, edge_index)
+    out = self.elu(out)
+    out = self.conv6(out, edge_index)
+    out = self.dropout05(out)
+    out = self.classifier(out)
     return out
 
-model = Network(n_hidden_channels=3)
+model = Network(n_hidden_channels=55)
 print(model)
 
 # -- Training loop --
 losses = []
 loss = None
 # TODO: consider using Hamming Loss as a loss function
-criterion = torch.nn.MSELoss()  # Initialize the loss function.
+criterion = torch.nn.L1Loss()  # Initialize the loss function.
+# TODO: consider some other than Adam optimizer
 optimizer = torch.optim.Adam(params=model.parameters(), lr=0.01)
+
+model.train()
+
+X = torch.tensor([[-1], [0], [0], [0], [0], [2], [0], [0], [0], [0], [0]], dtype=torch.float, requires_grad=True)# delete after testing 
+y = torch.tensor([[0], [1], [4], [5], [-1], [-1], [-1], [-1], [-1], [-1], [-1]], dtype=torch.float)# delete after testing 
+edge_index = torch.tensor([[0, 1, 0, 7, 1, 4, 7, 4, 1, 2, 4, 5, 7, 8, 4, 5, 5, 10, 2, 3, 3, 6, 6, 2, 8, 10, 6, 10, 9, 3], [1, 0, 7, 0, 4, 1, 4, 7, 2, 1, 5, 4, 8, 7, 5, 4, 10, 5, 3, 2, 6, 3, 2, 6, 10, 8, 10, 6, 3, 9]], dtype=torch.long)# delete after testing 
+out = model(X, edge_index)# delete after testing 
+print(f'1!!! Result: {out}', "\n", f"Expected result: {y}") # delete after testing 
 
 for epoch in range(n_epochs):
   # One epoch
   for cur_batch_index, batch in enumerate(trainLoader):
     # One batch
     optimizer.zero_grad()
+
     out = model(batch.x, batch.edge_index)
     loss = criterion(out, batch.y)
     loss.backward()
 
     optimizer.step()  # Update parameters based on gradients.
 
-    if cur_batch_index % 5 == 0:
+    if cur_batch_index % 25 == 0:
       losses.append(loss.item())
-
-    print(f"Batch {cur_batch_index}, last loss = {losses[-1]:.4f}")
+      print(f"Batch {cur_batch_index}, last loss = {losses[-1]:.4f}")
+    
+    if cur_batch_index % 100 == 0: # delete after testing 
+      out = model(X, edge_index) # delete after testing 
+      print(f'Result: {out}', "\n", f"Expected result: {y}") # delete after testing 
 
 # -- Validation -- 
 # losses = []
